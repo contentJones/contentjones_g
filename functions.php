6ee2434389891acb049520e860c0c2d4cadbe95d
contentJones <pd@pauldavidson.info>
2015-06-07 08:07:48 -0600

    initial commit of child theme.

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+/*
+//load the parent theme stylesheet and then the child theme stylesheet	
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+*/
