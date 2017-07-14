55943eb6cf28004a504c31d5cb9b99c97537e2d4
contentJones <pd@pauldavidson.info>
2017-07-14 16:07:57 -0600

    Oh my. Been a while since I commited changes on this. Comitt catchup!

--- front-page.php
+++ front-page.php
@@ -1,14 +1,47 @@
 <?php
 	
+
+	
 //ADD HOMEPAGE CONTENT
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'theme_homepage_content');
 function theme_homepage_content() {
 	?>
-	<!-- ******** PORTFOLIO ******** -->
+
+	<!-- ******** BLOG ******** -->
+	<section class="blog clearfix">
+		
+		<h2 class="subhead">recent blog posts</h2>
+	
+		<?php
+			
+			// The Query
+			wp_reset_query();
+			$query = new WP_Query( array( 'category_name' => 'blog') );
+			if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+		
+			<article>
+					<header class="entry-header">
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<div class="entry-meta">
+							<p>Posted: <?php the_date(); ?></p>
+						</div><!-- .entry-meta -->
+					</header><!-- .entry-header -->
+					
+					<?php // the_excerpt(); ?>
+					
+			</article>
+
+		<!-- end The Query -->
+		<?php endwhile; 
+			  endif; ?>
+			
+	</section>
+	
+		<!-- ******** PORTFOLIO ******** -->
 	<section class="portfolio">
 		
-	<h2 class="subhead">recent work</h2>		
+	<h2 class="subhead">portfolio of work</h2>		
 
 		<!-- begin loop -->
 		<?php
@@ -31,18 +64,46 @@ function theme_homepage_content() {
 				<div class="featured-excerpt">
 					<h3><a href="<?php echo get_post_meta( get_the_ID(), 'work-link', true ); ?>" target="_blank"><?php echo get_the_title(); ?></a></h3>
 					<p><?php the_content(); ?></p>
-<!-- 					<a class="button" href="#">Visit the site</a> -->
 				</div>
 				<div class="featured-image">
+
 					<!-- get full-sized featured image and link to the media file -->
+<!--
 					<?php if ( has_post_thumbnail() ) {
 								$full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-								echo '<a href="' . $full_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+								echo '<a href="' . $full_image_url[0] . '" rel="lightbox" title="' . the_title_attribute( 'echo=0' ) . '">';
 								the_post_thumbnail( 'full' );
 								echo '</a>';
 							} ?>
-				</div>
+-->
+					<ul class="bxslider override clearfix">
+						<?php
+							
+							global $post;
+							
+							 $image_args = array(
+							 	'post_type' => 'attachment',
+							 	'numberposts' => -1,
+							 	'order' => 'ASC',
+							 	'post_status' => null,
+							 	'post_parent' => $post->ID
+							 	);
+							 
+							 $attachments = get_posts( $image_args );
+							   if ( $attachments ) {
+							      foreach ( $attachments as $attachment ) {
+							         echo '<li>';
+							         echo wp_get_attachment_image( $attachment->ID, 'full' );
+							         echo '</li>';
+							        }
+							   }
+							   ?>
+					</ul>
+				</div>			
 			</div>
+			
+			
+			
 		</div>
 				
 		<!-- end of loop -->
@@ -51,38 +112,20 @@ function theme_homepage_content() {
 		
 	</section>		
 
-	<!-- ******** BLOG ******** -->
-	<section class="blog">
-		
-		<h2 class="subhead">recent posts</h2>
 	
-		<?php
-			
-			// The Query
-			wp_reset_query();
-			$query = new WP_Query( array( 'category_name' => 'blog') );
-			if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-		
-			<article>
-					<header class="entry-header">
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-						<div class="entry-meta">
-							<p>Posted on <?php the_date(); ?></p>
-						</div><!-- .entry-meta -->
-					</header><!-- .entry-header -->
-					
-					<?php the_excerpt(); ?>
-					
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="button">Read on...</a>
-			</article>
-
-		<!-- end The Query -->
-		<?php endwhile; 
-			  endif; ?>
-			
-	</section>
 	<?php
 }
+
+function initialize_bxslider(){ ?>
+	<script>
+		jQuery(document).ready(function(){
+		  jQuery('.bxslider').bxSlider({
+			  maxSlides: 1,
+		  });
+		});
+	</script>
+<?php } 
+add_action('wp_footer', 'initialize_bxslider', 1);
 	
 	
 genesis();
--- functions.php
+++ functions.php
@@ -5,8 +5,20 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 
 // ENQUEUE SCRIPTS AND STYLES
 function contentjones_g_scripts() {
+	//load Google fonts
+	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Fredoka+One|Lato:300,400i', array(), '1.1', 'all');
+
 	//load main.js and jQuery
-	wp_enqueue_script( 'main',  get_stylesheet_directory_uri() . '/js/main.js', array('jquery-core'), '1.0.0', true );
+	wp_enqueue_script( 'main',  get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), '1.0.0', true );
+
+
+	if ( is_front_page() ) {
+		//load bxslider script + css
+		wp_enqueue_script( 'bxslider', 'https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js', array('jquery'), '4.2.12', true );
+		wp_enqueue_style( 'bxslider', 'https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.css', array(), '4.2.12', 'all');
+		}
+
+	
 }
 add_action( 'wp_enqueue_scripts', 'contentjones_g_scripts' );
 
@@ -38,3 +50,15 @@ function theme_offcanvas_close() {
 	<?php
 }
 
+//Change search placeholder
+
+function theme_search_button_text( $text ) {
+return ( 'Search...');
+}
+add_filter( 'genesis_search_text', 'theme_search_button_text' );
+
+//Display a custom favicon
+add_filter( 'genesis_pre_load_favicon', 'theme_favicon_filter' );
+function theme_favicon_filter( $favicon_url ) {
+	return get_stylesheet_directory_uri() .'/favicon.png';
+}
